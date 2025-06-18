a771a038940f60d1b677b43391b3e7b1a8389e6a
jkrzacz <j.krzacz@gmail.com>
2025-06-18 20:07:42 +0200

    Update MainActivity.java

--- app/src/main/java/com/spark/coolbutton/MainActivity.java
+++ app/src/main/java/com/spark/coolbutton/MainActivity.java
@@ -1,9 +1,10 @@
 package com.spark.coolbutton;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity; // ← używamy AndroidX
 
 import com.spark.submitbutton.SubmitButton;
 
@@ -13,9 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SubmitButton sb = (SubmitButton) findViewById(R.id.btn3);
+
+        SubmitButton sb = findViewById(R.id.btn3);
         sb.setOnClickListener(new View.OnClickListener() {
-            public static final String TAG = "onclick";
+            private static final String TAG = "onclick";
 
             @Override
             public void onClick(View v) {
